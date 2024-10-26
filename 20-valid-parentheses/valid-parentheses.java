57f6cad4e16e521c56462bf7982250349d99ff31
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2024-10-26 15:58:28 -0400

    Time: 3 ms (44.66%) | Memory: 41.5 MB (59.54%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,57 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='[')
+            {
+                st.push(s.charAt(i));
+            }
+            else
+            {
+             if(st.isEmpty())
+                {
+                    return false;
+                }
+            if(s.charAt(i)==')')
+            {
+               
+                if(st.peek()=='(')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+             if(s.charAt(i)=='}')
+            {
+                if(st.peek()=='{')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+             if(s.charAt(i)==']')
+            {
+                if(st.peek()=='[')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+             
+            }
+        }
+        if(!st.isEmpty())
+          return false;
+        return true;
+    }
+}
