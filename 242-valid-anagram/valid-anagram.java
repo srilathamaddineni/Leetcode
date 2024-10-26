3b00399a6200aa8dfa95a970e1c5ca5b90c7d69b
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2024-10-26 13:21:12 -0400

    Time: 23 ms (8.89%) | Memory: 44.7 MB (28.54%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(!map.containsKey(s.charAt(i)))
+            {
+                map.put(s.charAt(i),1);
+            }
+            else
+            {
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            if(!map.containsKey(t.charAt(i)) || map.get(t.charAt(i))==0)
+            {
+                return false;
+            }
+            else
+            {
+                map.put(t.charAt(i),map.get(t.charAt(i))-1);
+            }
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(map.get(s.charAt(i))!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
