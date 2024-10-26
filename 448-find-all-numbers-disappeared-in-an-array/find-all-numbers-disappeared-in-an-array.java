6d41e9966879e7a80d829918ff96d5cdebf727cf
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2024-10-25 20:36:43 -0400

    Time: 5 ms (81.74%) | Memory: 53.4 MB (89.33%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,9 +1,11 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        /*
+        //Solution with using the extra space
         List<Integer> res=new ArrayList<>();
         int n=nums.length;
         Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++)
+        for(int i=1;i<nums.length;i++)
         {
            map.putIfAbsent(nums[i],1);
         }
@@ -19,6 +21,28 @@ class Solution {
             res.add(n);
         }
 
+      return res;*/
+      //Solution Without using the extra space
+      List<Integer> res=new ArrayList<>();
+      for(int i=0;i<nums.length;i++)
+      {
+         int j=nums[i];
+         if(j<0)
+         {
+            j*=-1;
+         }
+         if(nums[j-1]>0)
+         {
+            nums[j-1]*=-1;
+         }
+      }
+      for(int i=0;i<nums.length;i++)
+      {
+        if(nums[i]>0)
+        {
+            res.add(i+1);
+        }
+      }
       return res;
     }
 }
