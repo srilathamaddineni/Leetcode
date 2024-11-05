a16a7101279e91ab1e82f5ec6fee0d76e971adf7
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2024-11-05 10:41:36 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (41.44%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        //Brute Force
+        /*Brute Force
         int idx=-1;
         for(int i=0;i<nums.length;i++)
         {
@@ -8,6 +8,40 @@ class Solution {
             {
                 idx=i;
                 break;
+            }
+        }
+        return idx;*/
+        //Optimized Solution using Binary Search
+        int n=nums.length,low=0,high=n-1;
+        int idx=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]>=nums[low])
+            {
+                if(nums[low]<=target && target<=nums[mid])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                     low=mid+1;
+                }
+            }
+            else
+            {
+                if(nums[mid]<=target && target<=nums[high])
+                {
+                   low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
             }
         }
         return idx;
