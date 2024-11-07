4abdd872e79044de39178e339857e8e8f2e3f250
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2024-11-06 22:35:25 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (39.61%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       
-        int m=matrix.length;
+       //Brute Force
+        /*int m=matrix.length;
         for(int i=0;i<m;i++)
         {
            int n=matrix[i].length;
@@ -14,6 +14,51 @@ class Solution {
               }
            }
         }
+        return false;*/
+        //Optimized Solution
+        int m=matrix.length;     
+        int low=0;
+        int high=m-1;
+        if (matrix.length == 0 || matrix[0].length == 0) {
+             return false;
+        }
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int n=matrix[mid].length;
+            if(matrix[mid][0]>target)
+            {
+                high=mid-1;
+            }
+            else if(matrix[mid][n-1]<target)
+            {
+                low=mid+1;
+            }
+            else
+            { 
+               int low1=0, high1=n-1;
+               while(low1<=high1)
+             { 
+               int mid1=(low1+high1)/2;
+               if(matrix[mid][mid1]==target)
+               {
+                  return true;
+               }
+               else if(matrix[mid][mid1]>target)
+               {
+                 high1=mid1-1;
+               }
+               else
+               {
+                  low1=mid1+1;
+               }
+
+            }
+            return false;
+            }
+            
+        }
         return false;
+
     }
 }
