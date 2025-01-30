6f5e8d1f40de564e7dbcfec05a244bcc8d65f2c4
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-01-29 20:55:33 -0500

    Time: 6 ms (97.96%) | Memory: 47.6 MB (90.16%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,43 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-       /* List<List<String>> ans=new ArrayList<>();
-        List<String> li=new ArrayList<>();
-        li.add(strs[0]);
-        ans.add(li);
-        for(int i=1;i<strs.length;i++)
-        { 
-            int flag=0;
-            for(int j=0;j<ans.size();j++)
-            {
-                char[] arr1=strs[i].toCharArray();
-                char[] arr2=ans.get(j).get(0).toCharArray();
-                Arrays.sort(arr1);
-                Arrays.sort(arr2);
-                if(Arrays.equals(arr1,arr2))
-                {
-                    ans.get(j).add(strs[i]);
-                    flag=1;
-                }
-            }
-          if(flag==0)
-          {
-             List<String> temp=new ArrayList<>();
-             temp.add(strs[i]);
-             ans.add(temp);
-          }
-        }
-        return ans;*/
-        Map<String,List<String>> sortMapping=new HashMap<>();
-        List<List<String>> ans=new ArrayList<>();
+        Map<String,List<String>> map=new HashMap<>();
+        List<List<String>> result=new ArrayList<>();
         for(int i=0;i<strs.length;i++)
         {
-            char sortedArray[]=strs[i].toCharArray();
+            char[] sortedArray=strs[i].toCharArray();
             Arrays.sort(sortedArray);
             String sorted=new String(sortedArray);
-            sortMapping.computeIfAbsent(sorted,k->new ArrayList<>()).add(strs[i]);
+            map.computeIfAbsent(sorted,k->new ArrayList<>()).add(strs[i]);
         }
-        ans.addAll(sortMapping.values());
-        return ans;
-      
+        result.addAll(map.values());
+        return result;
     }
 }
