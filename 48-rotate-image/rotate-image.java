2153cb3581203ec1f420fac3242e544d36c46547
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-01-30 00:14:48 -0500

    Time: 0 ms (100.00%) | Memory: 42.5 MB (14.51%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        //transpose
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=i+1;j<matrix[0].length;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        //reverse
+        for(int i=0;i<matrix.length;i++)
+        {
+            int left=0,right=matrix.length-1;
+            while(left<right)
+            {
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+        
+    }
+}
