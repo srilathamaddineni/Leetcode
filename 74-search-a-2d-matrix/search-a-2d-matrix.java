9b4f7ff50f861b56d331bf22e06774614f48fdab
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-01-30 12:08:11 -0500

    Time: 0 ms (100.00%) | Memory: 42.4 MB (28.75%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,64 +1,38 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-       //Brute Force
-        /*int m=matrix.length;
-        for(int i=0;i<m;i++)
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int top=0;
+        int bottom=n-1;
+        int left=0;
+        int right=m-1;
+        while(top<=bottom)
         {
-           int n=matrix[i].length;
-           if(matrix[i][n-1]>=target && matrix[i][0]<=target)
-           {
-              for(int j=0;j<n;j++)
-              {
-                 if(matrix[i][j]==target)
-                    return true;
-              }
-           }
-        }
-        return false;*/
-        //Optimized Solution
-        int m=matrix.length;     
-        int low=0;
-        int high=m-1;
-        if (matrix.length == 0 || matrix[0].length == 0) {
-             return false;
-        }
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            int n=matrix[mid].length;
-            if(matrix[mid][0]>target)
+            int row_mid=top+(bottom-top)/2;
+            int col_mid=left+(right-left)/2;
+            if(matrix[row_mid][col_mid]==target)
+               return true;
+            else if(matrix[row_mid][col_mid]<target)
             {
-                high=mid-1;
+                for(int j=col_mid+1;j<=right;j++)
+                {
+                    if(matrix[row_mid][j]==target)
+                       return true;
+                }
+                top=row_mid+1;
             }
-            else if(matrix[mid][n-1]<target)
+             else
             {
-                low=mid+1;
-            }
-            else
-            { 
-               int low1=0, high1=n-1;
-               while(low1<=high1)
-             { 
-               int mid1=(low1+high1)/2;
-               if(matrix[mid][mid1]==target)
-               {
-                  return true;
-               }
-               else if(matrix[mid][mid1]>target)
-               {
-                 high1=mid1-1;
-               }
-               else
-               {
-                  low1=mid1+1;
-               }
-
-            }
-            return false;
+                for(int j=0;j<col_mid;j++)
+                {
+                    if(matrix[row_mid][j]==target)
+                       return true;
+                }
+                
+                bottom=row_mid-1;
             }
             
         }
-        return false;
-
+       return false;
     }
 }
