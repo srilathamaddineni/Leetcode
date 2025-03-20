cf5e79c6b702f350f3c7dbb20214902f8e91e568
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-03-19 21:51:48 -0400

    Time: 1 ms (99.88%) | Memory: 53.3 MB (38.24%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,37 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n=nums.length;
-        int ans=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++)
+        // Questions to be asked
+        //    Are all the numbers positive
+        //    length of the array range
+    //     int n=nums.length;
+    //     int ans=0;
+    //    HashMap<Integer,Integer> map=new HashMap<>();
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+    //         if(map.get(nums[i])>n/2)
+    //         {
+    //             return nums[i];
+    //         }
+    //     }
+    //     return ans;
+       int cnt=0;
+       int candidate=0;
+       for(int num:nums)
+       {
+        if(cnt==0)
         {
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(map.get(nums[i])>n/2)
-            {
-                return nums[i];
-            }
+            candidate=num;
         }
-        return ans;
+        if(num==candidate)
+        {
+            cnt++;
+        }
+        else
+        {
+            cnt--;
+        }
+       }
+       return candidate;
     }
 }
