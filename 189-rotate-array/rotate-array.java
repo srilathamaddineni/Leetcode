b4a3fde02e46ffc710c0f03e8613b33cb7f5b610
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-03-20 20:54:12 -0400

    Time: 0 ms (100.00%) | Memory: 57.1 MB (90.22%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,18 +1,36 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        // int n=nums.length;
+        // // int rotatedArr[]=new int[n];
+        // if(k>n)
+        // {
+        //     k=(k-n)%n;
+        // }
+        // // for(int i=0;i<nums.length;i++)
+        // // {
+        // //     rotatedArr[i]=nums[(n+i-k)%n];
+        // // } 
+        // // for(int i=0;i<nums.length;i++)
+        // // {
+        // //     nums[i]=rotatedArr[i];
+        // // }
+        //In-Place
         int n=nums.length;
-        int rotatedArr[]=new int[n];
-        if(k>n)
+        k=k%n;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+       
+    }
+    public void reverse(int arr[],int left,int right)
+    {
+        while(left<right)
         {
-            k=(k-n)%n;
-        }
-        for(int i=0;i<nums.length;i++)
-        {
-            rotatedArr[i]=nums[(n+i-k)%n];
-        } 
-        for(int i=0;i<nums.length;i++)
-        {
-            nums[i]=rotatedArr[i];
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
         }
     }
 }
