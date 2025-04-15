920fef5f863b74eac3e03206fd31ee2617425304
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-04-14 21:44:24 -0400

    Time: 6 ms (86.14%) | Memory: 43.3 MB (46.12%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,35 +1,62 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//       if(s==null || s.isEmpty())
+//       {
+//         return s;
+//       }
+//       int start=0;
+//       int end=s.length()-1;
+//       StringBuilder reversedStr=new StringBuilder();
+
+//       while(s.charAt(start)==' ')
+//       {
+//          start++;
+//       } 
+//       while(s.charAt(end)==' ')
+//       {
+//         end--;
+//       } 
+//      while(end>=start)
+//       {
+//         String word="";
+//          while(end>=start && s.charAt(end)!=' ')
+//          {
+//             word=s.charAt(end)+word;
+//             end--; 
+//          }
+//         if (reversedStr.length() > 0 && s.charAt(end+1)!=' ') {
+//                 reversedStr.append(" ");
+//             }
+//          reversedStr.append(word);
+//          end--;
+//       }   
+//      return reversedStr.toString();
+//     }
+// }
 class Solution {
     public String reverseWords(String s) {
-      if(s==null || s.isEmpty())
-      {
-        return s;
-      }
-      int start=0;
-      int end=s.length()-1;
-      StringBuilder reversedStr=new StringBuilder();
+        String[] words = s.split(" ");
+        int left = 0;
+        int right = words.length - 1;
 
-      while(s.charAt(start)==' ')
-      {
-         start++;
-      } 
-      while(s.charAt(end)==' ')
-      {
-        end--;
-      } 
-     while(end>=start)
-      {
-        String word="";
-         while(end>=start && s.charAt(end)!=' ')
-         {
-            word=s.charAt(end)+word;
-            end--; 
-         }
-        if (reversedStr.length() > 0 && s.charAt(end+1)!=' ') {
-                reversedStr.append(" ");
+        while (left < right) {
+            String temp = words[left];
+            words[left] = words[right];
+            words[right] = temp;
+            left++;
+            right--;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                if (result.length() > 0) {
+                    result.append(" ");
+                }
+                result.append(word);
             }
-         reversedStr.append(word);
-         end--;
-      }   
-     return reversedStr.toString();
+        }
+
+        return result.toString();
     }
 }
