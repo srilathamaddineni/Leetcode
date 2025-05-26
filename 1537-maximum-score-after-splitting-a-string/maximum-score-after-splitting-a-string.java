4cb1badb6256b880377988c02f4370dc32494a50
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-05-26 12:50:39 -0400

    Time: 2 ms (37.19%) | Memory: 41.5 MB (79.30%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -3,49 +3,15 @@ class Solution {
         int n=s.length();
         int left[]=new int[n];
         int right[]=new int[n];
-        for(int i=0;i<s.length();i++)
+        left[0]=s.charAt(0)=='0'?1:0;
+        right[n-1]=s.charAt(n-1)=='1'?1:0;
+        for(int i=1;i<n;i++)
         {
-            if(s.charAt(i)=='0')
-            {
-                if(i==0)
-                {
-                    left[i]=1;
-                }
-                else
-                {
-                    left[i]=left[i-1]+1;
-                }
-                
-            }
-            else
-            {
-                if(i!=0)
-                {
-                 left[i]=left[i-1];
-                }
-            }
+            left[i]=s.charAt(i)=='0'?left[i-1]+1:left[i-1];
         }
-        for(int i=n-1;i>=0;i--)
+        for(int i=n-2;i>=0;i--)
         {
-            if(s.charAt(i)=='1')
-            {
-               if(i==n-1)
-               {
-                  right[i]=1;
-               }
-               else
-               {
-                 right[i]=right[i+1]+1;
-               }
-            }
-            else
-            {
-                if(i!=n-1)
-                {
-                  right[i]=right[i+1];
-                }
-            }
-            
+            right[i]=s.charAt(i)=='1'?right[i+1]+1:right[i+1];
         }
         int max=Integer.MIN_VALUE;
         for(int i=0;i<n-1;i++)
