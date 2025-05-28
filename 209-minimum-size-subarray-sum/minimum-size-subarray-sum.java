e830c4c53fcb833aa90b03528802d0f3fa93232d
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-05-28 17:13:02 -0400

    Time: 12 ms (5.35%) | Memory: 58.2 MB (53.58%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,23 +1,58 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left=0;
-        int minLength=Integer.MAX_VALUE;
-        int currSum=0;
-        for(int right=0;right<nums.length;right++)
+        // int left=0;
+        // int minLength=Integer.MAX_VALUE;
+        // int currSum=0;
+        // for(int right=0;right<nums.length;right++)
+        // {
+        //     currSum+=nums[right];
+        //     while(currSum>=target)
+        //     {
+        //         if(right-left+1<minLength)
+        //         {
+        //             minLength=right-left+1;
+        //         }
+        //         currSum-=nums[left];
+        //         left++;
+        //     }
+        // }
+        // return minLength==Integer.MAX_VALUE?0:minLength;
+        //Prefix+BinarySearch
+        int n=nums.length;
+        int prefix[]=new int[n+1];
+        for(int i=0;i<n;i++)
         {
-            currSum+=nums[right];
-            while(currSum>=target)
+            prefix[i+1]=prefix[i]+nums[i];
+        }
+        int minLength=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            int toFind=prefix[i]+target;
+            int bound=binarySearch(prefix,toFind);
+            if(bound!=-1)
             {
-                if(right-left+1<minLength)
-                {
-                    minLength=right-left+1;
-                }
-                currSum-=nums[left];
-                left++;
+                minLength=Math.min(minLength,bound-i);
             }
         }
         return minLength==Integer.MAX_VALUE?0:minLength;
-
-
+    }
+    public int binarySearch(int[] prefix,int target)
+    {
+        int left=0,right=prefix.length-1;
+        int result=-1;
+        while(left<=right)
+        {
+           int mid=left+(right-left)/2;
+           if(prefix[mid]>=target)
+           {
+              result=mid;
+              right=mid-1;
+           }
+           else
+           {
+              left=mid+1;
+           }
+        }
+        return result;
     }
 }
