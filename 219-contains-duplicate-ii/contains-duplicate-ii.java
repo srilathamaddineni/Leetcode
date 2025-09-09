def19d905278ba3b8e7f278e03c4b11d865c086b
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-09-09 15:53:54 -0400

    Time: 16 ms (99.33%) | Memory: 59.5 MB (81.88%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,29 +1,17 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if(k<=nums.length)
-        {
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=i+1;j-i<=k && j<nums.length;j++)
-            {
-                if(nums[i]==nums[j] && Math.abs(i-j)<=k)
-                {
-                    return true;
-                }
-            }
-        }
-        }
-        else
-        {
-            Arrays.sort(nums);
-            for(int i=0;i<nums.length-1;i++)
-            {
-                if(nums[i]==nums[i+1])
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
+       Set<Integer> set=new HashSet<>();
+       for(int i=0;i<nums.length;i++)
+       {
+         if(i>k)
+         {
+             set.remove(nums[i-k-1]);
+         }
+         if(!set.add(nums[i]))
+         {
+            return true;
+         }
+       }
+       return false;
     }
 }
