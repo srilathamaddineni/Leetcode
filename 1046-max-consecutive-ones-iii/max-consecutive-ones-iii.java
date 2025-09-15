e16bacad9d65199ce3c7369974eaef5cc4059c23
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-09-15 16:04:28 -0400

    Time: 3 ms (96.08%) | Memory: 53.2 MB (27.66%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -20,23 +20,37 @@ class Solution {
         int maxLength=0;
         int l=0;
         int zeroes=0;
-        for(int r=0;r<nums.length;r++)
-        {
-           if(nums[r]==0)
-           {
-            zeroes++;
-           }
-           while(zeroes>k)
-           {
-              if(nums[l]==0)
-              {
-                zeroes--;
-              }
-              l++;
-           }
-            maxLength=Math.max(maxLength,r-l+1);
+    //     for(int r=0;r<nums.length;r++)
+    //     {
+    //        if(nums[r]==0)
+    //        {
+    //         zeroes++;
+    //        }
+    //        while(zeroes>k)
+    //        {
+    //           if(nums[l]==0)
+    //           {
+    //             zeroes--;
+    //           }
+    //           l++;
+    //        }
+    //         maxLength=Math.max(maxLength,r-l+1);
             
-        }
+    //     }
+    //   return maxLength;
+    /////////////////removing while loop////////////////////
+      int r=0;
+      while(r<nums.length)
+      {
+         if(nums[r]==0)zeroes++;
+         if(zeroes>k)
+         {
+            if(nums[l]==0)zeroes--;
+            l++;
+         }
+         if(zeroes<=k)maxLength=Math.max(maxLength,r-l+1);
+         r++;
+      }
       return maxLength;
 
     }
