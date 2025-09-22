d12756e352bfdb19b0211d8d2c07725894fd9d7c
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-09-22 16:05:27 -0400

    Time: 1 ms (19.16%) | Memory: 46.1 MB (31.23%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,67 +1,22 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        /*Brute Force
-        int n=nums.length;
-        int left=-1;
-        int right=-1;
-        
-        for(int i=0;i<n;i++)
+        int arr[]={-1,-1};
+        for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==target)
             {
-                left=i;
-                break;
+                if(arr[0]==-1)
+                {
+                    arr[0]=i;
+                    arr[1]=i;    
+                }
+                else
+                {
+                    arr[1]=i;
+                }
             }
         }
-        for(int i=n-1;i>=0;i--)
-        {
-            if(nums[i]==target)
-            {
-                right=i;
-                break;
-            }
-        }
-        int res[]=new int[2];
-        res[0]=left;
-        res[1]=right;
-        return res;*/
-      // Binary Search
-      int n=nums.length;
-      int res[]={-1,-1};
-      res[0]=firstOccurence(nums,target);
-      res[1]=lastOccurence(nums,target);
-      return res;       
-    }
-    public int firstOccurence(int nums[],int target)
-    {
-        int low=0,high=nums.length-1,first=-1;
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            {
-                first=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target)low=mid+1;
-            else high=mid-1;
-        }
-        return first;
-    }
-    public int lastOccurence(int nums[],int target)
-    {
-        int low=0,high=nums.length-1,last=-1;
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            {
-                last=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<target)low=mid+1;
-            else high=mid-1;
-        }
-        return last;
+        return arr;
+       
     }
 }
