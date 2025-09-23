7b6934f79f4113f05aa7256b61c8be9cdf344368
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-09-23 19:59:47 -0400

    Time: 0 ms (100.00%) | Memory: 42.6 MB (43.17%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,19 +1,46 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        // int n=nums.length;
+        // if(n==1)
+        // {
+        //     return 0;
+        // }
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     boolean isLeftGreater=(i==0 || nums[i]>nums[i-1]);
+        //     boolean isRightGreater=(i==n-1 || nums[i]>nums[i+1]);
+        //     if(isLeftGreater && isRightGreater)
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return -1;
         int n=nums.length;
         if(n==1)
+          return 0;
+        if(nums[0]>nums[1])
+           return 0;
+        if(nums[n-1]>nums[n-2])
+           return n-1;
+        int low=1;
+        int high=n-2;
+        while(low<=high)
         {
-            return 0;
-        }
-        for(int i=0;i<nums.length;i++)
-        {
-            boolean isLeftGreater=(i==0 || nums[i]>nums[i-1]);
-            boolean isRightGreater=(i==n-1 || nums[i]>nums[i+1]);
-            if(isLeftGreater && isRightGreater)
+            int mid=low+(high-low)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
             {
-                return i;
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1])
+            {
+               low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
             }
         }
         return -1;
+
     }
 }
