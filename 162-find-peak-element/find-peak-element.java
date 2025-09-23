6ab5e63a65c843457757e909f3e0ca0580aa626a
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-09-23 18:49:41 -0400

    Time: 0 ms (100.00%) | Memory: 42.6 MB (43.17%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -5,17 +5,11 @@ class Solution {
         {
             return 0;
         }
-        if(nums[0]>nums[1])
+        for(int i=0;i<nums.length;i++)
         {
-            return 0;
-        }
-        if(nums[n-1]>nums[n-2])
-        {
-            return n-1;
-        }
-        for(int i=1;i<nums.length-1;i++)
-        {
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
+            boolean isLeftGreater=(i==0 || nums[i]>nums[i-1]);
+            boolean isRightGreater=(i==n-1 || nums[i]>nums[i+1]);
+            if(isLeftGreater && isRightGreater)
             {
                 return i;
             }
