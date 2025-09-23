a86da14f2ea9331dde2af11a07db9797ebbbc76f
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-09-23 18:45:14 -0400

    Time: 0 ms (100.00%) | Memory: 42.6 MB (43.17%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,9 +1,7 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        /*Brute Force
         int n=nums.length;
-        int idx=0;
-        if(nums.length<=1 )
+        if(n==1)
         {
             return 0;
         }
@@ -15,31 +13,13 @@ class Solution {
         {
             return n-1;
         }
-        for(int i=1;i<n-1;i++)
+        for(int i=1;i<nums.length-1;i++)
         {
             if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
             {
-                idx=i;
-                break;
+                return i;
             }
         }
-        return idx;*/
-        //Optimized Approach
-        int left=0;
-        int right=nums.length-1;
-        while(left<right)
-        {
-            int mid=(left+right)/2;
-            if(nums[mid]>nums[mid+1])
-            {
-                right=mid;
-            }
-            else
-            {
-                left=mid+1;
-            }
-        }
-        return left;
-
+        return -1;
     }
 }
