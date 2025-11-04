b1cd31dc1236a89e4bb30ee266203100d93fbf33
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-11-04 17:34:46 -0500

    Time: 4 ms (12.57%) | Memory: 46.7 MB (5.26%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -10,25 +10,55 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        l1=reverse(l1);
-        l2=reverse(l2);
-        ListNode dummy=new ListNode(0);
-        ListNode curr=dummy;
+        // l1=reverse(l1);
+        // l2=reverse(l2);
+        // ListNode dummy=new ListNode(0);
+        // ListNode curr=dummy;
 
-        int carry=0;
-        while(l1!=null || l2!=null || carry!=0)
+        // int carry=0;
+        // while(l1!=null || l2!=null || carry!=0)
+        // {
+        //     int val1=(l1==null)?0:l1.val;
+        //     int val2=(l2==null)?0:l2.val;
+        //     int val3=val1+val2+carry;
+        //     carry=val3/10;
+        //     curr.next=new ListNode(val3%10);
+        //     curr=curr.next;
+        //     if(l1!=null)l1=l1.next;
+        //     if(l2!=null)l2=l2.next;
+        // }
+
+        // return reverse(dummy.next);
+
+        //Using Stacks
+        Stack<Integer> st1=new Stack<>();
+        Stack<Integer> st2=new Stack<>();
+        while(l1!=null)
         {
-            int val1=(l1==null)?0:l1.val;
-            int val2=(l2==null)?0:l2.val;
+            st1.push(l1.val);
+            l1=l1.next;
+        }
+        while(l2!=null)
+        {
+            st2.push(l2.val);
+            l2=l2.next;
+        }
+        ListNode head=null;
+        int carry=0;
+        while(!st1.isEmpty() || !st2.isEmpty() || carry!=0)
+        {
+            int val1=(st1.isEmpty())?0:st1.peek();
+            int val2=(st2.isEmpty())?0:st2.peek();
             int val3=val1+val2+carry;
             carry=val3/10;
-            curr.next=new ListNode(val3%10);
-            curr=curr.next;
-            if(l1!=null)l1=l1.next;
-            if(l2!=null)l2=l2.next;
-        }
+            ListNode newNode=new ListNode(val3%10);
+            newNode.next=head;
+            head=newNode;
+            if(!st1.isEmpty())st1.pop();
+            if(!st2.isEmpty())st2.pop();
 
-        return reverse(dummy.next);
+        }
+        return head;
 
     }
     public ListNode reverse(ListNode l1)
