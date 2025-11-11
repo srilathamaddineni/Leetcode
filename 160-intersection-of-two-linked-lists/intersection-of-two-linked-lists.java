c512e6ff2d21ea3941f83d06d37127a78cd62dd5
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-11-11 18:41:23 -0500

    Time: 1 ms (99.88%) | Memory: 47.5 MB (98.73%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,20 +11,62 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode,Integer> map=new HashMap<>();
+        //Brute Froce
+        // HashMap<ListNode,Integer> map=new HashMap<>();
+        // ListNode temp1=headA;
+        // ListNode temp2=headB;
+        // while(temp1!=null)
+        // {
+        //     map.put(temp1,1);
+        //     temp1=temp1.next;
+        // }
+        // while(temp2!=null)
+        // {
+        //     if(map.containsKey(temp2))
+        //     {
+        //        return temp2;
+        //     }
+        //     temp2=temp2.next;
+        // }
+        // return null;
         ListNode temp1=headA;
         ListNode temp2=headB;
+        int cnt1=0, cnt2=0;
         while(temp1!=null)
         {
-            map.put(temp1,1);
+            cnt1++;
             temp1=temp1.next;
         }
         while(temp2!=null)
         {
-            if(map.containsKey(temp2))
+            cnt2++;
+            temp2=temp2.next;
+        }
+        temp1=headA;
+        temp2=headB;
+        if(cnt1>cnt2)
+        {
+            while(cnt1>cnt2 && temp1!=null)
             {
-               return temp2;
+                temp1=temp1.next;
+                cnt1--;
             }
+        }
+        else
+        {
+            while(cnt2>cnt1 && temp2!=null)
+            {
+                temp2=temp2.next;
+                cnt2--;
+            }
+        }
+        while(temp1!=null && temp2!=null)
+        {
+            if(temp1==temp2)
+            {
+                return temp1;
+            }
+            temp1=temp1.next;
             temp2=temp2.next;
         }
         return null;
