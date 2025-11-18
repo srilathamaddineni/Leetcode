13126d6667662147b60814da66f0de1016af6f04
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-11-18 17:20:37 -0500

    Time: 4 ms (31.63%) | Memory: 43 MB (27.13%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,25 +3,35 @@ class Solution {
         Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++)
         {
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='[')
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='[')
             {
-                st.push(s.charAt(i));
+                st.push(ch);
             }
             else
             {
-             if(st.isEmpty())
+                if(st.isEmpty())
                 {
                     return false;
                 }
-                char top=st.pop();
-              if ((s.charAt(i) == ')' && top != '(') || 
-                    (s.charAt(i) == '}' && top != '{') || 
-                    (s.charAt(i)== ']' && top != '[')) {
-                    return false;
+                else
+                {
+                    if((ch==')' && st.peek()=='(') || ch=='}' && st.peek()=='{' || ch==']' && st.peek()=='[') 
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
             }
         }
-    }
-       
-        return st.isEmpty();
+        if(!st.isEmpty())
+        {
+            return false;
+        }
+        return true;
     }
 }
