695eb256a0ccc5017cd2af0500b0e66ef2223ff3
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2025-11-20 14:13:13 -0500

    Time: 1 ms (57.89%) | Memory: 48.8 MB (6.18%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,55 +1,28 @@
 class Solution {
     public int trap(int[] height) {
-    //     int n=height.length;
-    //     int maxLeft[]=new int[n];
-    //     int maxRight[]=new int[n];
-    //     int amtOfWater=0;
-    //     maxLeft[0]=0;
-    //     maxRight[n-1]=0;
-    //     int left=height[0];
-    //     int right=height[n-1];
-    //     for(int i=1;i<n;i++)
-    //     {
-    //        maxLeft[i]=Math.max(maxLeft[i-1],left);
-    //        left=height[i];
-    //     }
-    //     for(int i=n-2;i>=0;i--)
-    //     {
-    //         maxRight[i]=Math.max(maxRight[i+1],right);
-    //         right=height[i];
-    //     }
-    //     for(int i=1;i<n-1;i++)
-    //     {
-    //         int currValue=Math.min(maxLeft[i],maxRight[i])-height[i];
-    //         if(currValue>0)
-    //         {
-    //             amtOfWater+=currValue;
-    //         }
-    //     }
+        int n=height.length;
+        int leftMax[]=new int[n];
+        int rightMax[]=new int[n];
+        leftMax[0]=height[0];
+        rightMax[n-1]=height[n-1];
+        int unitsOfWater=0;
+        for(int i=1;i<n;i++)
+        {
+           leftMax[i]=Math.max(leftMax[i-1],height[i]);
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            rightMax[i]=Math.max(rightMax[i+1],height[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(height[i]<leftMax[i] && height[i]<rightMax[i])
+            {
+              unitsOfWater+=Math.min(leftMax[i],rightMax[i])-height[i];
+            }
 
-    //   return amtOfWater;
-    int n=height.length;
-     int leftMax=height[0];
-     int rightMax=height[n-1];
-     int left=0;
-     int right=n-1;
-     int res=0;
-     while(left<right)
-     {
-           if(leftMax<rightMax)
-           {
-             left+=1;
-             leftMax=Math.max(leftMax,height[left]);
-             res+=leftMax-height[left];
-           }
-           else
-           {
-             right--;
-             rightMax=Math.max(rightMax,height[right]);
-             res+=rightMax-height[right];
-           }
-     }
-     return res;
+        }
+       return unitsOfWater;
 
     }
 }
