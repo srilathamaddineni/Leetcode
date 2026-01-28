5f662e7d5cf2f3e1cdbf65b0c5027f916d46d128
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2026-01-27 19:16:13 -0500

    Time: 1 ms (12.30%) | Memory: 43.9 MB (11.37%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -16,19 +16,37 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
      
-      return  symmetrical(root.left,root.right);
-
-    }
-    public boolean symmetrical(TreeNode n1,TreeNode n2)
-    {
-        if(n1==null && n2==null)
-        {
-            return true;
+      Queue<TreeNode> queue=new LinkedList<>();
+      queue.add(root.left);
+      queue.add(root.right);
+      while(!queue.isEmpty())
+      {
+        TreeNode n1=queue.poll();
+        TreeNode n2=queue.poll();
+        if(n1==null && n2==null){
+            continue;
         }
-        if(n1==null || n2==null)
+        if(n1==null || n2==null || n1.val!=n2.val)
         {
             return false;
         }
-        return n1.val==n2.val && symmetrical(n1.left,n2.right) && symmetrical(n1.right,n2.left);
+        queue.add(n1.left);
+        queue.add(n2.right);
+        queue.add(n1.right);
+        queue.add(n2.left);
+      }
+     return true;
     }
+    // public boolean symmetrical(TreeNode n1,TreeNode n2)
+    // {
+    //     if(n1==null && n2==null)
+    //     {
+    //         return true;
+    //     }
+    //     if(n1==null || n2==null)
+    //     {
+    //         return false;
+    //     }
+    //     return n1.val==n2.val && symmetrical(n1.left,n2.right) && symmetrical(n1.right,n2.left);
+    // }
 }
