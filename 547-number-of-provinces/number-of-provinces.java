19991b44a5972eafd304497266888af776f54815
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2026-02-17 18:47:53 -0500

    Time: 4 ms (20.45%) | Memory: 49.6 MB (13.60%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,46 +1,46 @@
 class Solution {
-   public void dfs(int i,List<List<Integer>> adjList,boolean[] vis)
-   {
-       vis[i]=true;
-       for(int j:adjList.get(i))
+    public void dfs(int node,List<List<Integer>> adjList,boolean visited[])
+    {
+        visited[node]=true;
+        
+        for(int j:adjList.get(node))
        {
-          if(!vis[j])
+          if(!visited[j])
           {
-             dfs(j,adjList,vis);
+             dfs(j,adjList,visited);
           }
        }
-
-   }
-
-
+        
+        
+    }
     public int findCircleNum(int[][] isConnected) {
-         List<List<Integer>> adjList=new ArrayList<>();
-         int v=isConnected.length;
-         int cnt=0;
-         boolean vis[]=new boolean[v+1];
-         vis[0]=true;
-         for(int i=0;i<=v;i++)
-         {
+        List<List<Integer>> adjList=new ArrayList<>();
+        int v=isConnected.length;
+        int cnt=0;
+        boolean visited[]=new boolean[v+1];
+        visited[0]=true;
+        for(int i=0;i<=v;i++)
+        {
             adjList.add(new ArrayList<>());
-         }
-         for(int i=0;i<v;i++ )
-         {
+        }
+        for(int i=0;i<v;i++)
+        {
             for(int j=0;j<isConnected[i].length;j++)
             {
-                if(isConnected[i][j]==1 && i!=j)
-                {
+               if(isConnected[i][j]==1 && i!=j)
+               {
                   adjList.get(i+1).add(j+1);
-                }
+               }
             }
-         }
-         for(int i=1;i<=v;i++)
-         {
-            if(!vis[i])
+        }
+        for(int i=1;i<=v;i++)
+        {
+            if(!visited[i])
             {
                 cnt++;
-                dfs(i,adjList,vis);
+                dfs(i,adjList,visited);
             }
-         }
-         return cnt;
+        }
+     return cnt;
     }
 }
