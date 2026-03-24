92079e196673f3fc06377da9324ea07a77f81fad
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2026-03-24 16:12:55 -0400

    Time: 8 ms (91.05%) | Memory: 48.8 MB (93.40%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,32 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int row=intervals.length;
-        Arrays.sort(intervals, (a, b) ->Integer.compare(a[0],b[0]));
-        int res[][]=new int[row][2];
-        res[0][0]=intervals[0][0];
-        res[0][1]=intervals[0][1];
-        int j=0;
-        for(int i=1;i<row;i++)
-        {
-           if(intervals[i][0]<=res[j][1])
-           {
-             res[j][1]=Math.max(intervals[i][1],res[j][1]);
-           }
-           else
-           {
-              j++;
-              res[j][0]=intervals[i][0];
-              res[j][1]=intervals[i][1];
-           } 
-        }
-  
-        int finRes[][]=new int[j+1][2];
-        for(int i=0;i<=j;i++)
-        {
-           finRes[i][0]=res[i][0];
-           finRes[i][1]=res[i][1];
-
-        }
-        return finRes;
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int[][] result=new int[intervals.length][2];
+         int idx=0;
+         result[0]=intervals[0];
+         for(int i=1;i<intervals.length;i++)
+         {
+            if(result[idx][1]>=intervals[i][0])
+            {
+                result[idx][1]=Math.max(intervals[i][1],result[idx][1]);
+            }
+            else
+            {
+                result[++idx]=intervals[i];
+            }
+         }
+         return Arrays.copyOfRange(result,0,idx+1);
     }
 }
