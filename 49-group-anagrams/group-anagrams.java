ea196899fe93afc8384306aacdfcad2b5325c881
Srilatha Maddineni <51310262+srilathamaddineni@users.noreply.github.com>
2026-03-29 23:45:37 -0400

    Time: 19 ms (19.13%) | Memory: 50.4 MB (15.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,9 +3,20 @@ class Solution {
         Map<String,List<String>> map=new HashMap<>();
         for(String str:strs)
         {
-           char[] chars=str.toCharArray();
-           Arrays.sort(chars);
-           String key=new String(chars);
+        //    char[] chars=str.toCharArray();
+        //    Arrays.sort(chars);
+        //    String key=new String(chars);
+           int[] count=new int[26];
+           for(char c:str.toCharArray())
+           {
+              count[c-'a']++;
+           }
+           StringBuilder sb=new StringBuilder();
+           for(int num:count)
+           {
+            sb.append(num).append("#");
+           }
+           String key=sb.toString();
            if(!map.containsKey(key))
            {
               map.put(key,new ArrayList<>());
